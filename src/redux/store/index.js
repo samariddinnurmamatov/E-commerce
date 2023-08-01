50760d13574734cd5d4d476092cc350e20296a79
“Samariddin <samariddinnurmamatov111@gmail.com>
2023-08-01 09:50:10 +0500

    E-commerce

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,9 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer, { nameCounter } from "../slices/counterSlice";
 import productReducer, { productsName } from "../slices/productsSlice";
 
 const reducer = {
-  [nameCounter]: counterReducer,
   [productsName]: productReducer,
 };
 
